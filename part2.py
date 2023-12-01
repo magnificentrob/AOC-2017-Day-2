1c66c9325ad0dbc868172cebda4873c0ca26cf4d
magnificentrob <31416219+magnificentrob@users.noreply.github.com>
2023-12-01 15:00:14 -0500

    Update part2.py

--- part2.py
+++ part2.py
@@ -4,9 +4,10 @@ x = 0
 with open('input.txt', newline='') as csvfile:
 	reader = csv.reader(csvfile, delimiter='\t')
 	for row in reader:
-		checkSumSheet.append(row)
+		anew =[]
 		for i in range(len(row)):
-			row[i] = int(row[i])
+			anew.append(int(row[i]))
+		checkSumSheet.append(row)
 		checkSumSheet[x] = sorted(checkSumSheet[x], reverse=True)
 		x+=1
 
